603c6685b4a920209d0d4aff181323192746efed
hcaillaud <hector.caillaud@ign.fr>
2024-04-10 15:11:28 +0200

    carte centrée et ajout de menu de navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-useScheme()
-import useToaster from './composables/use-toaster'
+import { useRoute } from 'vue-router'
 
-const toaster = useToaster()
+useScheme()
 
 // Paramètres pour le Header
 const serviceTitle = 'Carte.gouv.fr'
@@ -11,24 +10,24 @@ const logoText = ['Ministère', 'de l’intérieur']
 
 const quickLinks = [
   {
-    label: 'Home',
-    to: '/',
-    icon: 'ri-home-4-line',
-    iconAttrs: { color: 'var(--red-marianne-425-625)' },
-  },
-  {
     label: serviceDescription,
-    to: '/carte',
+    to: '/',
     icon: 'ri-arrow-right-line',
     iconRight: true
   },
   {
-    label: 'À propos',
-    to: '/a-propos',
-    icon: 'ri-question-mark',
+    label: 'Catalogue',
+    to: '/catalogue',
+    icon: 'ri-arrow-right-line',
     iconRight: true,
   },
+  {
+    label: 'Se Connecter',
+    to: '/login',
+    icon: 'fa-user-circle',
+  }
 ]
+
 const searchQuery = ref('')
 
 // Paramètres pour le Footer
@@ -73,7 +72,175 @@ const ecosystemLinks = [
   },
 ]
 
+// Paramètre pour la barre de navigation
+const route = useRoute()
 
+const navItems: DsfrNavigationProps['navItems'] = [
+  {
+    title: 'Présentation',
+    get active () {
+      return ['Présentation générale', 'Gouvernance', 'Commencer sur cartes.gouv.fr'].includes(route.name as string)
+    },
+    links: [
+      {
+        href: '',
+        text: 'Présentation générale',
+      },
+      {
+        href: '',
+        text: 'Gouvernance',
+      },
+      {
+        href: '',
+        text: 'Commencer sur cartes.gouv.fr',
+      },
+    ],
+  },
+  {
+    title: 'Ressources',
+    get active () {
+      return ['Catalogue de données (prochainement)', 'Catalogue de traitements (prochainement)', 'API & outils de construction (prochainement)'].includes(route.name as string)
+    },
+    links: [
+      {
+        href: '',
+        text: 'Catalogue de données (prochainement)',
+      },
+      {
+        href: '',
+        text: 'Catalogue de traitements (prochainement)',
+      },
+      {
+        href: '',
+        text: 'API & outils de construction (prochainement)',
+      },
+    ],
+  },
+  {
+    title: 'Développeurs',
+    get active () {
+      return ['Blog Développeurs', 'Communautés de développeurs', 'API & outils de construction (prochainement)', 'Bac à sable développement', 'Gitlab cartes.gouv'].includes(route.name as string)
+    },
+    links: [
+      {
+        href: '',
+        text: 'Blog Développeurs',
+      },
+      {
+        href: '',
+        text: 'Communautés de développeurs',
+      },
+      {
+        href: '',
+        text: 'API & outils de construction (prochainement)',
+      },
+      {
+        href: '',
+        text: 'Bac à sable développement',
+      },
+      {
+        href: '',
+        text: 'Gitlab cartes.gouv',
+      },
+    ],
+  },
+
+  {
+    title: 'Communautés géoplateforme',
+    get active () {
+      return ['Organisations (prochainement)', 'Communautés d\'assistance', 'Réutilisations', 'Témoignages'].includes(route.name as string)
+    },
+    links: [
+      {
+        href: '',
+        text: 'Organisations (prochainement)',
+      },
+      {
+        href: '',
+        text: 'Communautés d\'assistance',
+      },
+      {
+        href: '',
+        text: 'Réutilisations',
+      },
+      {
+        href: '',
+        text: 'Témoignages',
+      }
+    ],
+  },
+  {
+    to: { href: '' },
+    text: 'Actualités',
+  },
+  {
+    to: { href: '' },
+    text: 'Agenda',
+  },
+  {
+    title: 'Support',
+    get active () {
+      return ['Niveau de service', 'Documentation', 'Didactitiels', 'Contacter l\'assistance', 'Plans d\'assistance', 'FAQ'].includes(route.name as string)
+    },
+    links: [
+      {
+        href: '',
+        text: 'Niveau de service',
+      },
+      {
+        href: '',
+        text: 'Documentation',
+      },
+      {
+        href: '',
+        text: 'Didactitiels',
+      },
+      {
+        href: '',
+        text: 'Contacter l\'assistance',
+      },
+      {
+        href: '',
+        text: 'Plans d\'assistance',
+      },
+      {
+        href: '',
+        text: 'FAQ',
+      },
+    ],
+  }
+]
+// Paramètre pour la barre de navigation
+const categoryName1 = 'Présentation'
+const linkList1 = [
+  {
+    to: { href: '' },
+    label: 'Présentation générale',
+  },
+  {
+    to: { href: '' },
+    label: 'Gouvernance',
+  },
+  {
+    to: { href: '' },
+    label: 'Commencer sur cartes.gouv.fr',
+  },
+]
+const categoryName2 = 'Ressources'
+const linkList2 = [
+  {
+    to: { href: '' },
+    label: 'Présentation générale',
+  },
+  {
+    to: { href: '' },
+    label: 'Gouvernance',
+  },
+  {
+    to: { href: '' },
+    label: 'Commencer sur cartes.gouv.fr',
+  },
+]
 </script>
 
 <template>
@@ -84,9 +251,15 @@ const ecosystemLinks = [
     :logo-text="logoText"
     :quick-links="quickLinks"
     show-search
-  />
+  >
+    <!-- <template #mainnav> -->
+    <DsfrNavigation
+      :nav-items="navItems"
+    />
+    <!-- </template> -->
+  </DsfrHeader>
 
-  <div class="fr-container  fr-mt-3w  fr-mt-md-5w  fr-mb-5w">
+  <div>
     <router-view />
   </div>
 
@@ -106,5 +279,18 @@ const ecosystemLinks = [
     :licence-name="licenceName"
     :licence-link-props="licenceLinkProps"
     :ecosystem-links="ecosystemLinks"
-  />
+  >
+    <template #footer-link-lists>
+      <DsfrFooterLinkList
+        class="fr-col-6"
+        :category-name="categoryName1"
+        :links="linkList1"
+      />
+      <DsfrFooterLinkList
+        class="fr-col-6"
+        :category-name="categoryName2"
+        :links="linkList2"
+      />
+    </template>
+  </DsfrFooter>
 </template>
--- src/views/portailCarto.vue
+++ src/views/portailCarto.vue
@@ -1,19 +1,16 @@
-
 <script setup lang="ts">
-import vlMap from "./ol-views/vlMap.vue";
-import vlView from "./ol-views/vlView.vue";
+import vlMap from './ol-views/vlMap.vue'
+import vlView from './ol-views/vlView.vue'
 
-const zoom =  10;
-const center = [-234814.550892, 4774562.534805];
-const mapID = "map";
+const zoom = 10
+const center = [-234814.550892, 4774562.534805]
 </script>
-
 
 <template>
   <vlMap>
-    <vlView 
+    <vlView
       :center="center"
       :zoom="zoom"
-      />
+    />
   </vlMap>
 </template>
