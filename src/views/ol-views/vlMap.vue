c4c93003265b7a959968f8ae8d0c49270c783efc
hcaillaud <hector.caillaud@ign.fr>
2024-04-15 15:32:38 +0200

    🌟 keyboard control on map

--- src/views/ol-views/vlMap.vue
+++ src/views/ol-views/vlMap.vue
@@ -18,7 +18,21 @@ const map: Map | undefined = new Map({
 
 onMounted(() => {
   map?.setTarget(mapRef.value)
+
+  const canvas = document.getElementById('map')?.getElementsByTagName('canvas')
+  if (canvas) {
+    canvas.tabindex = 0
+  }
 })
+
+/**
+ *  To focus on map and activate keyboard control.
+ *  Trigerred on mouse over
+ *
+ */
+function focusOnMap () {
+  mapRef.value.focus()
+}
 
 provide('map', map)
 </script>
@@ -27,6 +41,8 @@ provide('map', map)
   <div
     id="map"
     ref="mapRef"
+    tabindex="0"
+    @mouseover="focusOnMap"
   >
     <slot />
   </div>
@@ -36,6 +52,6 @@ provide('map', map)
 #map {
   margin-left: 0;
   width: 100vw;
-  height: 100vh;
+  height: 80vh;
 }
 </style>
