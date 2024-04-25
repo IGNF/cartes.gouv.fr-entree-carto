9ac850f69579c79e9d3f0cb0dc106d35dba0a55b
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-04-25 18:04:27 +0200

    init map store

--- src/components/carte/control/SearchEngine.vue
+++ src/components/carte/control/SearchEngine.vue
@@ -3,7 +3,8 @@
 import { useLogger } from 'vue-logger-plugin'
 import { storeToRefs } from 'pinia'
 import { useDataStore } from "@/stores/dataStore"
-import SearchEngine from 'geoportal-extensions-openlayers/src/packages/Controls/SearchEngine/SearchEngine'
+
+import { SearchEngine } from 'geoportal-extensions-openlayers'
 
 const props = defineProps({
   visibility: Boolean,
@@ -21,11 +22,7 @@ onMounted(() => {
   if (props.visibility) {
     map.addControl(searchEngine.value)
     // abonnement au widget
-    searchEngine.value.on("searchengine:search:click", (e) => {
-      if (store.isLoaded) {
-        log.debug("search", e.suggest, getLayerByName.value(e.suggest.name, e.suggest.service));
-      }
-    });
+    searchEngine.value.on("searchengine:search:click", onClickSearch);
   }
 })
 
@@ -40,6 +37,16 @@ onUpdated(() => {
     map.addControl(searchEngine.value)
   }
 })
+
+/** 
+ * gestionnaire d'evenement sur l'abonnement à la recherche de couche
+ */
+const onClickSearch = (e) => {
+  if (store.isLoaded) {
+      log.debug("search", e.suggest, toRaw(getLayerByName.value(e.suggest.name, e.suggest.service)));
+  }
+}
+
 </script>
 
 <template></template>
--- src/stores/mapStore.js
+++ src/stores/mapStore.js
@@ -2,6 +2,31 @@ import { defineStore } from 'pinia'
 
 /**
  * Store des objets de la carte
- * @todo à mettre en place
+ * Enregistrement dans le LocalStorage
+ * @todo à completer
  */
-export const useMapStore = defineStore('map', {});
+export const useMapStore = defineStore('map', {
+  state: () => ({
+    map: null,
+    zoom: 0,
+    center: []
+  }),
+  getters: {
+    getMap: (state) => state.map,
+    getZoom: (state) => state.zoom,
+    getCenter: (state) => state.center
+  },
+  actions: {
+    setMap(map) {
+      this.map = map;
+    },
+    setZoom(zoom) {
+      this.zoom = zoom;
+      localStorage.setItem('zoom', zoom)
+    },
+    setCenter(center) {
+      this.center = center;
+      localStorage.setItem('center', center)
+    }
+  }
+});
