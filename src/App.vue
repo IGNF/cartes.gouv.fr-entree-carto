ce4b5428005e0e1503deb16a9f3696d4a93b3a09
elias couppe <elias.couppe@gmail.com>
2024-05-07 15:53:47 +0200

    feat(footer): implemtation param display theme

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,54 @@ import LogoIGN from "./assets/logo-ign.png"
 import LogoTRANSFO from "./assets/logo-transformation-fonction-publiques.png"
 import LogoECOLO from "./assets/logo-transition-ecologique.png"
 import LogoCNIG from "./assets/logo-cnig.png"
+import { useModel } from 'vue';
 
 // Paramètres de mediaQuery pour affichage HEADER et FOOTER
 const largeScreen = useMatchMedia("LG");
+
+const { setScheme, theme } = useScheme()
+
+const openModalTheme = () => {
+  opened.value = true;
+  // setScheme(theme.value === 'light' ? 'dark' : 'light')
+}
+const onModalClose = () => {
+  opened.value = false;
+  // setScheme(theme.value === 'light' ? 'dark' : 'light')
+}
+const modelValue = ref()
+
+const changeTheme = () => {
+  setScheme(modelValue.value);
+}
+
+// Parametres Modale Theme
+const opened = ref(false);
+const title = "Params"
+const size = "md"
+
+const legend = "Groupe de cases à cocher simple"
+const themeOptions = [
+  {
+    label: 'Thème clair',
+    value: 'light',
+    id: 'theme-1',
+    name: 'radio-set',
+  },
+  {
+    label: 'Thème Sombre',
+    id: 'theme-2',
+    value: 'dark',
+    name: 'radio-set',
+  },
+  {
+    label: 'Système',
+    id: 'theme-3',
+    value: 'system',
+    name: 'radio-set',
+    hint: 'Utilise les paramètres sytème.',
+  }
+]
 
 // Paramètres pour le Header
 const serviceTitle = 'cartes.gouv.fr'
@@ -43,6 +88,7 @@ const afterMandatoryLinks = [
     button: true,
     class: 'fr-icon-theme-fill fr-link--icon-left fr-px-2v',
     to: '/settings',
+    onclick: openModalTheme
   },
 ]
 const beforeMandatoryLinks = [{ label: 'Plan du site', to: '/plan-du-site' }]
@@ -109,7 +155,7 @@ const route = useRoute()
 const navItems: DsfrNavigationProps['navItems'] = [
   {
     title: 'Commencer avec cartes.gouv',
-    get active () {
+    get active() {
       return ['Documentation Géoplateforme', 'Questions fréquentes', 'Nous écrire'].includes(route.name as string)
     },
     links: [
@@ -139,47 +185,34 @@ const navItems: DsfrNavigationProps['navItems'] = [
 </script>
 
 <template>
-  <DsfrHeader
-    v-model="searchQuery"
-    :service-title="serviceTitle"
-    :service-description="serviceDescription"
-    :logo-text="logoText"
-    :quick-links="quickLinks"
-  >
-    <DsfrNavigation
-      :nav-items="navItems"
-      v-show="!largeScreen"
-    />
+  <DsfrHeader v-model="searchQuery" :service-title="serviceTitle" :service-description="serviceDescription"
+    :logo-text="logoText" :quick-links="quickLinks">
+    <DsfrNavigation :nav-items="navItems" v-show="!largeScreen" />
   </DsfrHeader>
 
   <Suspense>
-    <StoreDataLoading/>
+    <StoreDataLoading />
     <!-- loading state -->
     <template #fallback>
       Loading...
     </template>
   </Suspense>
-  
+
   <div>
     <router-view />
   </div>
-  
-  <DsfrFooter
-    :before-mandatory-links = "beforeMandatoryLinks"
-    :after-mandatory-links="afterMandatoryLinks"
-    :a11y-compliance="a11yCompliance"
-    :logo-text="logoText"
-    :legal-link="legalLink"
-    :personal-data-link="personalDataLink"
-    :cookies-link="cookiesLink"
-    :a11y-compliance-link="a11yComplianceLink"
-    :desc-text="descText"
-    :home-link="homeLink"
-    :partners="partners"
-    :licence-text="licenceText"
-    :licence-to="licenceTo"
-    :licence-name="licenceName"
-    :licence-link-props="licenceLinkProps"
-    :ecosystem-links="ecosystemLinks"
-  />
+
+  <DsfrFooter :before-mandatory-links="beforeMandatoryLinks" :after-mandatory-links="afterMandatoryLinks"
+    :a11y-compliance="a11yCompliance" :logo-text="logoText" :legal-link="legalLink" :personal-data-link="personalDataLink"
+    :cookies-link="cookiesLink" :a11y-compliance-link="a11yComplianceLink" :desc-text="descText" :home-link="homeLink"
+    :partners="partners" :licence-text="licenceText" :licence-to="licenceTo" :licence-name="licenceName"
+    :licence-link-props="licenceLinkProps" :ecosystem-links="ecosystemLinks" />
+  <DsfrModal ref="modal" :opened="opened" :title="title" :size="size" @close="onModalClose">
+    <div class="fr-container fr-my-2v">
+      <div class="fr-my-2v">
+        <DsfrRadioButtonSet v-model="modelValue" :legend="legend" name="radio-set" :options="themeOptions"
+          @update:model-value="changeTheme" />
+      </div>
+    </div>
+  </DsfrModal>
 </template>
