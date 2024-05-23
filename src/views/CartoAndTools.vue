427974ea03e6159e8fad83bdb7832bd84a63ac4a
hcaillaud <hector.caillaud@ign.fr>
2024-05-23 17:49:38 +0200

    🚧 les menus ne font plus partie du composant carto

---
+++ src/views/CartoAndTools.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import Carto from '@/components/carte/Carto.vue'
+import Patience from '@/components/utils/Patience.vue'
+import StoreDataLoading from '@/components/StoreDataLoading.vue';
+import LeftMenu from '@/components/menu/LeftMenu.vue'
+import RightMenu from '@/components/menu/RightMenu.vue'
+
+import { useControls } from '@/composables/controls'
+import { useDataStore } from "@/stores/dataStore"
+
+const dataStore = useDataStore()
+const catalogueProps = { layersConf : toRaw(dataStore.layers) };
+
+const availableControls = Object.values(useControls);
+const selectedControls = ref(availableControls);
+
+const newLayer = ref("");
+const layersList = computed(() => {
+  let list = listBeforeUpdate.value
+  if (newLayer.value)
+    list.push(newLayer.value)
+  list = list.map(layername => getLayerConfFromTitle(layername))
+  return list;
+})
+// FIXME c'est pour l'exemple, on utilise cette couche par defaut !?
+// mais, les settings par defaut devraient être fournis par le mapStore !
+const listBeforeUpdate = ref(["Plan IGN v2"])
+
+// FIXME c'est une methode du dataStore !
+function getLayerConfFromTitle(layername) {
+  return Object.values(catalogueProps.layersConf).filter((layer) => {
+    if (layername && layer.title == layername) {
+      return layer
+    }
+  })[0]
+}
+
+function addLayer(layername) {
+  newLayer.value = layername;
+  listBeforeUpdate.value.concat(layername)
+}
+
+
+const mapWidth=70
+const menuWidth = 100 - mapWidth;
+</script>
+
+<template>
+<div id="map-and-tools-container">
+  <LeftMenu
+    :width="menuWidth"
+    :catalogue-props="catalogueProps"
+    v-model="selectedControls"
+     @catalogue-event="addLayer"
+    />
+  <RightMenu
+    :width="menuWidth"
+    :catalogue-props="catalogueProps"
+    v-model="selectedControls"
+     @catalogue-event="addLayer"
+    />
+  <Carto
+    :layers-list="layersList"
+    :selected-controls="selectedControls"
+    :map-width="mapWidth"/>
+</div>
+
+</template>
+
+<style scoped>
+  #map-and-tools-container{
+    margin-left: 0;
+    width: inherit;
+    height: 70vh;
+  }
+</style>
