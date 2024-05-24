bab95a315db19624b9b15adefea04857c9386d87
hcaillaud <hector.caillaud@ign.fr>
2024-05-24 20:31:02 +0200

    🌟 amelioration du menu lateral : resizable

--- src/components/menu/MenuLateralWrapper.vue
+++ src/components/menu/MenuLateralWrapper.vue
@@ -1,5 +1,7 @@
 <script setup lang="js">
 import { OhVueIcon as VIcon } from 'oh-vue-icons'
+import { useElementSize, useEventListener } from '@vueuse/core'
+import { vOnClickOutside } from '@vueuse/components'
 
 const props = defineProps({
   side: String,
@@ -15,7 +17,16 @@ const iconProps = computed(() => typeof icon === 'string'
 );
 
 const is_expanded = ref(false)
+const width = ref()
+const resizer = ref()
+width.value = props.width
+const cssWidth = computed(() => {
+  return width.value + "vw";
+})
+const menuTabs = ref()
+const { width : tabsWidth } = useElementSize(menuTabs)
 
+const isResizing = ref(false)
 const backgroundColor = getComputedStyle(document.body)?.backgroundColor;
 
 
@@ -23,28 +34,56 @@ const ToggleMenu = () => {
   is_expanded.value = !is_expanded.value
 }
 
+
+
+onMounted(() => {
+  const mousedown = useEventListener(resizer, 'mousedown', (e) => {
+    isResizing.value = true;
+  })
+  const mousemove = useEventListener(document, 'mousemove', (e) => {
+    if (isResizing.value) {
+      if (props.side == "left")
+        width.value =  100 * ((e.clientX) / window.innerWidth)
+      if (props.side == "right")
+        width.value =  100 * ((window.innerWidth - e.clientX - tabsWidth.value) / window.innerWidth)
+    }
+  })
+  const mouseup = useEventListener(document, 'mouseup', (e) => {
+    if (isResizing.value) {
+      isResizing.value = false;
+    }
+    
+  })
+})
+function closeMenu() {
+  is_expanded.value = false
+}
 </script>
 
 
 
 <template>
 
-  <div class="menu-toggle-wrap" :class="`${is_expanded  && 'is_expanded'} ${props.side}`">
+  <div class="menu-toggle-wrap" :class="`${is_expanded  && 'is_expanded'} ${props.side}`"
+    v-on-click-outside="closeMenu">
 
-    <div class="menu-content-list">
+    <div class="menu-content-list resizable"
+    draggable="false"
+    v-show="is_expanded">
         <slot name="content"></slot>
     </div>
 
-    <div class="menu-logo-list">
+    <div ref="menuTabs" class="menu-logo-list">
       <button class="menu-collapse-icon" @click="ToggleMenu">
         <VIcon
         v-bind="iconProps"/>  
       </button>
       <slot name="navButtons"></slot>
     </div>
-
-
-
+    <div class="panel-resizer"
+      ref="resizer"
+      v-show="is_expanded">
+    </div>
   </div>
 </template>
 
@@ -86,11 +125,12 @@ left: 0;
     position: absolute;
     height: inherit;
     display: inline-flex;
+    flex-flow: row wrap;
     z-index: 1;
     background-color: v-bind(backgroundColor);
     &.is_expanded {
       .menu-content-list {
-        width : auto;
+        width: v-bind(cssWidth);
       }
     }
 }
@@ -100,10 +140,27 @@ left: 0;
   width: 0;
   overflow-y: scroll;
   scrollbar-width: thin;
+  flex: 1;
 }
 .menu-logo-list {
   flex-direction: column;
   display: flex;
+}
+.resizable {
+  flex: none;
+  min-width: 0;
+  max-width: 50vw;
+}
+.panel-resizer {
+  cursor: col-resize;
+  position: relative;
+  width: 4px;
+  margin: 0 -2px;
+  z-index: 1;
+  &:hover{
+  background-color:#8585f6;
+  box-shadow: 0 1px 4px 1px #8585f6;
+}
 }
 
 
