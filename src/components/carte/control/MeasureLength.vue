7639349ac3f53a6b9a1568da36ef999750f1a36f
jean-philippe bazonnais <jean-philippe.bazonnais@ign.fr>
2024-07-05 15:21:03 +0200

    Ajout des outils de mesures (#256)

---
+++ src/components/carte/control/MeasureLength.vue
@@ -0,0 +1,32 @@
+<script setup lang="js">
+import { MeasureLength } from 'geoportal-extensions-openlayers'
+
+const props = defineProps({
+  visibility: Boolean,
+  measureLengthOptions: Object
+})
+
+const map = inject('map')
+const measureLength = ref(new MeasureLength(props.measureLengthOptions))
+
+onMounted(() => {
+  if (props.visibility) {
+    map.addControl(measureLength.value)
+  }
+})
+
+onBeforeUpdate(() => {
+  if (props.visibility) {
+    map.addControl(measureLength.value)
+  }
+  else {
+    map.removeControl(measureLength.value)
+  }
+})
+</script>
+
+<template>
+  <!-- TODO ajouter l'emprise du widget pour la gestion des collisions -->
+</template>
+
+<style></style>
