e3c31c88d64f36d6b7cbb87e5953cb49b8be7a95
hcaillaud <hector.caillaud@ign.fr>
2024-07-18 11:35:14 +0200

    🚧 maj du menu selon maquette

--- src/components/menu/MenuLateralWrapper.vue
+++ src/components/menu/MenuLateralWrapper.vue
@@ -46,7 +46,11 @@ defineExpose({
   :class="`${is_expanded  && 'is_expanded'} ${props.side}`"
   v-if="visibility"
   >
+    <div ref="menuTabs" class="menu-logo-list">
+      <slot name="navButtons"></slot>
+    </div>
 
+  
     <div class="menu-content-list"
     v-show="is_expanded">
       <div class="menu-collapse-icon-wrapper">
@@ -57,11 +61,9 @@ defineExpose({
       </button>
       </div>
 
-      <slot name="content"></slot>
-    </div>
-
-    <div ref="menuTabs" class="menu-logo-list">
-      <slot name="navButtons"></slot>
+      <div class="menu-content">
+        <slot name="content"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -73,22 +75,17 @@ defineExpose({
   .menu-logo-list {
 left: 7px;
   }
-  &.is_expanded {
-  .menu-logo-list {
-        transform: translateX(v-bind(cssWidth));
-      }
-    }
+  .menu-content-list {
+    left: 60px;
+  }
 }
 .right {
   .menu-logo-list {
   right: 56px;
   }
-  &.is_expanded {
-    flex-direction: row-reverse;
-    .menu-logo-list {
-        transform: translateX(v-bind(translateRight));
-      }
-}
+  .menu-content-list {
+    right: 60px;
+  }
 }
 
 .menu-collapse-icon {
@@ -106,7 +103,6 @@ left: 7px;
 
 .menu-toggle-wrap {
     height: inherit;
-    display: inline-flex;
     z-index: 1;
     &.is_expanded {
       .menu-content-list {
@@ -118,10 +114,16 @@ left: 7px;
 
 .menu-content-list {
   height: inherit;
-  overflow-y: scroll;
-  scrollbar-width: thin;
   background-color: v-bind(backgroundColor);
   padding-left: 30px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+}
+.menu-content {
+  overflow-y: scroll;
+  scrollbar-width: thin;
+  overflow-x: hidden;
 }
 .menu-logo-list {
   flex-direction: column;
