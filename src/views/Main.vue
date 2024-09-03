01ce99e77ba0fd8b1feee792f5b76704a9350adb
amaury.zarzelli <amaury.zarzelli@ign.fr>
2024-09-03 11:25:35 +0200

    feat(mobile): small footer

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { type DsfrNavigationProps } from '@gouvminint/vue-dsfr';
-import { useRoute } from 'vue-router';
-import { useMatchMedia } from '@/composables/matchMedia';
-import { useHeaderParams } from '@/composables/headerParams';
-import { useFooterParams } from '@/composables/footerParams';
-import { useBaseUrl } from '@/composables/baseUrl';
+import type { DsfrNavigationProps } from '@gouvminint/vue-dsfr'
+import { useRoute } from 'vue-router'
 import { useLogger } from 'vue-logger-plugin'
+import { useMatchMedia } from '@/composables/matchMedia'
+import { useHeaderParams } from '@/composables/headerParams'
+import { useFooterParams } from '@/composables/footerParams'
+import { useBaseUrl } from '@/composables/baseUrl'
 // components
 import ModalConsent from '@/components/modals/ModalConsent.vue'
 import ModalTheme from '@/components/modals/ModalTheme.vue'
@@ -13,16 +13,16 @@ import ModalTheme from '@/components/modals/ModalTheme.vue'
 const log = useLogger()
 
 // paramètres de mediaQuery pour affichage HEADER et FOOTER
-const largeScreen = useMatchMedia("LG");
+const largeScreen = useMatchMedia('LG')
 
 // paramètres pour le Header
-const headerParams = useHeaderParams();
+const headerParams = useHeaderParams()
 
 // paramètres pour le Footer
-const footerParams = useFooterParams();
+const footerParams = useFooterParams()
 
 // ref sur le component ModalTheme
-const refModalTheme = ref(null);
+const refModalTheme = ref(null)
 
 // INFO
 // on met à jour les afterMandatoryLinks pour y ajouter des
@@ -36,105 +36,106 @@ const afterMandatoryLinks = computed(() => {
       to: '/settings',
       onclick: refModalTheme.value ? refModalTheme.value.openModalTheme : null
     },
-  ];
+  ]
 })
 
 // ref sur le component ModalConsent
-const refModalConsent = ref(null);
+const refModalConsent = ref(null)
 
 // INFO
 // on met à jour les mandatoryLinks pour y ajouter des
 // options sur la 'gestion des cookies'
 const mandatoryLinks = computed(() => {
   return footerParams.mandatoryLinks.map((element: any) => {
-    if (element.label === "Gestion des cookies") {
-      delete element.href;
-      element.onclick = refModalConsent.value ? refModalConsent.value.openModalConsent : null;
-      element.to = "/";
+    if (element.label === 'Gestion des cookies') {
+      delete element.href
+      element.onclick = refModalConsent.value ? refModalConsent.value.openModalConsent : null
+      element.to = '/'
     }
-    return element;
-  });
+    return element
+  })
 })
 
 // paramètre pour la barre de navigations
-const route = useRoute();
+const route = useRoute()
 
 const navItems: DsfrNavigationProps['navItems'] = [
   {
     title: 'Commencer avec cartes.gouv',
-    get active() {
+    get active () {
       return [
         'Documentation',
         'Offre',
-        'Nous rejoindre'].includes(route.name as string)
+        'Nous rejoindre'
+      ].includes(route.name as string)
     },
     links: [
       {
-        to: useBaseUrl() + '/documentation',
+        to: `${useBaseUrl()}/documentation`,
         text: 'Documentation',
       },
       {
-        to: useBaseUrl() + '/offre',
+        to: `${useBaseUrl()}/offre`,
         text: 'Offre',
       },
       {
-        to: useBaseUrl() + '/nous-rejoindre',
+        to: `${useBaseUrl()}/nous-rejoindre`,
         text: 'Nous rejoindre',
       },
     ],
   },
   {
-    to: useBaseUrl() + '/catalogue',
+    to: `${useBaseUrl()}/catalogue`,
     text: 'Catalogue',
   },
   {
-    to: useBaseUrl() + '/actualites',
+    to: `${useBaseUrl()}/actualites`,
     text: 'Actualités',
   },
   {
     title: 'Assistance',
-    get active() {
+    get active () {
       return [
         'Questions fréquentes',
         'Nous écrire',
-        'Niveau de service'].includes(route.name as string)
+        'Niveau de service'
+      ].includes(route.name as string)
     },
     links: [
       {
-        to: useBaseUrl() + '/faq',
+        to: `${useBaseUrl()}/faq`,
         text: 'Questions fréquentes',
       },
       {
-        to: useBaseUrl() + '/nous-ecrire',
+        to: `${useBaseUrl()}/nous-ecrire`,
         text: 'Nous écrire',
       },
       {
-        to: useBaseUrl() + '/niveau-de-service',
+        to: `${useBaseUrl()}/niveau-de-service`,
         text: 'Niveau de service',
       },
     ],
   },
   {
-    to: useBaseUrl() + '/a-propos',
-    text: '\u00c0 propos',
+    to: `${useBaseUrl()}/a-propos`,
+    text: '\u00C0 propos',
   }
-];
-
+]
 </script>
 
 <template>
-  <DsfrHeader 
-    v-model="headerParams.serviceTitle" 
+  <DsfrHeader
+    v-model="headerParams.serviceTitle"
     :service-title="headerParams.serviceTitle"
-    :show-beta=true
-    :service-description="headerParams.serviceDescription" 
+    :show-beta="true"
+    :service-description="headerParams.serviceDescription"
     :logo-text="headerParams.logoText"
     :quick-links="headerParams.quickLinks"
   >
     <template #mainnav>
-      <DsfrNavigation 
+      <DsfrNavigation
         :nav-items="navItems"
-      /> 
+      />
     </template>
   </DsfrHeader>
 
@@ -142,40 +143,119 @@ const navItems: DsfrNavigationProps['navItems'] = [
     <router-view />
   </div>
 
-  <!-- INFO 
-      On retire les valeurs par defaut pour ajouter 
-      des valeurs customisées de mandatoryLinks.
-        :a11y-compliance="footerParams.a11yCompliance" 
-        :legal-link="footerParams.legalLink"
-        :personal-data-link="footerParams.personalDataLink" 
-        :cookies-link="footerParams.cookiesLink"
-        :a11y-compliance-link="footerParams.a11yComplianceLink"  
+  <!-- INFO
+      Bouton non DSFR pour laffichage du footer en mode mobile comme sur la maquette
   -->
-  <DsfrFooter 
-    :before-mandatory-links="footerParams.beforeMandatoryLinks" 
+  <label
+    class="fr-footer-toggle-label fr-btn fr-btn--tertiary-no-outline"
+    for="fr-footer-toggle"
+  />
+  <input
+    id="fr-footer-toggle"
+    type="checkbox"
+  >
+  <!-- INFO
+      On retire les valeurs par defaut pour ajouter
+      des valeurs customisées de mandatoryLinks.
+        :a11y-compliance="footerParams.a11yCompliance"
+        :legal-link="footerParams.legalLink"
+        :personal-data-link="footerParams.personalDataLink"
+        :cookies-link="footerParams.cookiesLink"
+        :a11y-compliance-link="footerParams.a11yComplianceLink"
+  -->
+  <DsfrFooter
+    :before-mandatory-links="footerParams.beforeMandatoryLinks"
     :after-mandatory-links="afterMandatoryLinks"
-    :logo-text="footerParams.logoText" 
+    :logo-text="footerParams.logoText"
     :desc-text="footerParams.descText"
-    :home-link="footerParams.homeLink" 
-    :partners="footerParams.partners" 
+    :home-link="footerParams.homeLink"
+    :partners="footerParams.partners"
     :licence-text="footerParams.licenceText"
-    :licence-to="footerParams.licenceTo" 
+    :licence-to="footerParams.licenceTo"
     :licence-name="footerParams.licenceName"
-    :licence-link-props="footerParams.licenceLinkProps" 
-    :ecosystem-links="footerParams.ecosystemLinks" 
+    :licence-link-props="footerParams.licenceLinkProps"
+    :ecosystem-links="footerParams.ecosystemLinks"
     :mandatory-links="mandatoryLinks"
   />
 
   <div class="fr-container fr-container--fluid fr-container-md">
-
     <!-- Modale : Paramètres d’affichage -->
     <ModalTheme ref="refModalTheme" />
 
     <!-- Modale : Gestion des cookies (+ Eulerian) -->
-    <ModalConsent ref="refModalConsent"/>
-
+    <ModalConsent ref="refModalConsent" />
   </div>
-
 </template>
 
-<style></style>
+<style>
+  #fr-footer-toggle {
+    display: none;
+  }
+  .fr-footer-toggle-label {
+    display: block;
+    position: absolute;
+    right: 0;
+    width: 32px;
+    min-height: 32px;
+    padding: 8px;
+    background-image: url(../assets/arrow-down.svg);
+    background-repeat: no-repeat;
+    background-position: center;
+    transform: translateY(12px);
+    transition: transform 0.2s;
+    caret-color: transparent;
+  }
+  .fr-footer-toggle-label:has(+ #fr-footer-toggle:checked) {
+    transform: translateY(12px) rotateX(180deg);
+  }
+  @media (min-width: 576px){
+    .fr-footer-toggle-label {
+      display: none;
+    }
+  }
+  @media (max-width: 576px){
+    .fr-footer {
+      padding: 0.5rem 0;
+    }
+    .fr-footer__body, .fr-footer__partners, .fr-footer__bottom > div {
+      display: none;
+    }
+    .fr-footer__partners + .fr-footer__bottom {
+      margin-top: 0;
+    }
+    .fr-footer__bottom > .fr-footer__bottom-list > .fr-footer__bottom-item::before {
+      display: none;
+    }
+    .fr-footer__bottom > .fr-footer__bottom-list > .fr-footer__bottom-item:not(:has(.fr-icon-theme-fill)) {
+      display: none;
+    }
+    .fr-footer__bottom {
+      box-shadow: unset;
+    }
+
+    #fr-footer-toggle:checked + .fr-footer {
+      padding-top: 2rem;
+    }
+
+    #fr-footer-toggle:checked + .fr-footer .fr-footer__body {
+      display: flex;
+    }
+
+    #fr-footer-toggle:checked + .fr-footer .fr-footer__partners,
+    #fr-footer-toggle:checked + .fr-footer .fr-footer__bottom > div {
+      display: unset;
+    }
+
+    #fr-footer-toggle:checked + .fr-footer .fr-footer__bottom > .fr-footer__bottom-list > .fr-footer__bottom-item::before {
+      display: inline-block;
+    }
+
+    #fr-footer-toggle:checked + .fr-footer .fr-footer__bottom > .fr-footer__bottom-list > .fr-footer__bottom-item:not(:has(.fr-icon-theme-fill)) {
+      display: inline-block;
+    }
+
+    #fr-footer-toggle:checked + .fr-footer .fr-footer__bottom {
+      box-shadow: inset 0 1px 0 0 var(--border-default-grey);
+    }
+  }
+</style>
