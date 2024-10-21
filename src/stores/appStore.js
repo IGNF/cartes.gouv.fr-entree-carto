256c1263bf51f58af45feb84c5de320082f2e35e
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-10-21 23:06:10 +0200

    Fix prise en compte du thème de cartes.gouv.fr + store applicatif (version, theme, ...)

---
+++ src/stores/appStore.js
@@ -0,0 +1,70 @@
+import {
+  defineStore
+} from 'pinia';
+
+import { useStorage } from '@vueuse/core';
+
+const { setScheme } = useScheme();
+
+/**
+ * Espace de noms des clefs du localStorage
+ */
+const NAMESPACE = "cartes.gouv.fr";
+
+const ns = ((value) => {
+  return NAMESPACE + '.' + value;
+});
+
+/**
+ * Versionning du localStorage
+ */
+const VERSION = "0";
+
+/**
+ * Clef du localStorage de cartes.gouv.fr
+ * sur le choix du thème
+ */
+const SCHEME_KEY_LS = "scheme";
+
+/**
+ * Clef du localStorage de cartes.gouv.fr/cartes
+ * sur le choix du thème
+ */
+const SCHEME_KEY_LS_CARTES = "vue-dsfr-scheme";
+
+export const useAppStore = defineStore('app', () => {
+
+  // INFO version
+  // la méthode useStorage() récupère la valeur dans le localStorage
+  // sinon, elle prend celle par defaut.
+  var version = useStorage(ns('version'), VERSION);
+
+  // si la version du localStorage est differente de celle du code,
+  // on nettoie le localStorage et on enregistre la bonne version.
+  if (version.value !== VERSION) {
+    localStorage.clear();
+    localStorage.setItem(ns('version'), VERSION);
+    version.value = VERSION;
+  }
+
+  watch(version, () => {
+    localStorage.setItem(ns('version'), version.value);
+  })
+
+  // INFO scheme color
+  // on récupère la valeur du thème de cartes.gouv.fr
+  var scheme = useStorage(SCHEME_KEY_LS);
+
+  // si cartes.gouv.fr a défini une valeur,
+  // on l'applique pour la carte
+  if (scheme.value) {
+    localStorage.setItem(SCHEME_KEY_LS_CARTES, scheme.value);
+    setScheme(scheme.value);
+  }
+  
+  watch(scheme, () => {
+    localStorage.setItem(SCHEME_KEY_LS_CARTES, scheme.value);
+    setScheme(scheme.value);
+  })
+
+});
