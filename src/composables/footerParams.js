878ca0b2be5ea326bb63409905d281db954dd424
hcaillaud <hector.caillaud@ign.fr>
2024-10-21 17:08:00 +0200

    🚑 footer link order fix

--- src/composables/footerParams.js
+++ src/composables/footerParams.js
@@ -32,16 +32,16 @@ export function useFooterParams() {
         licenceLinkProps: undefined,
         ecosystemLinks: [
             {
-                label: 'legifrance.gouv.fr',
-                href: 'https://legifrance.gouv.fr',
-            },
-            {
-                label: 'gouvernement.fr',
-                href: 'https://gouvernement.fr',
+                label: 'info.gouv.fr',
+                href: 'https://info.gouv.fr',
             },
             {
                 label: 'service-public.fr',
                 href: 'https://service-public.fr',
+            },
+            {
+                label: 'legifrance.gouv.fr',
+                href: 'https://legifrance.gouv.fr',
             },
             {
                 label: 'data.gouv.fr',
