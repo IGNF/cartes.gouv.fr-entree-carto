0f31c4ccf76ff3385991e1adbeb0ab957434a7e1
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-11-03 19:07:18 +0100

    Environnement + fonction generique fetch

---
+++ src/services/request.js
@@ -0,0 +1,45 @@
+/**
+ * @description
+ * Fonction générique pour executer une requête
+ * 
+ * @param {*} url - url
+ * @param {*} settings - configuration de la requête
+ * @example
+ * useRequest(url, { method: "GET"})
+ *   .then((res) => {})
+ *   .catch((e) => { console.error(e.message); });
+ */
+export async function useRequest(url, settings) {
+
+  // On peut ajouter les infos dans le header de la requête
+  // si on a une authentification
+  if (localStorage) {
+    var key = localStorage.getItem("auth");
+    if (key) {
+      var auth = JSON.parse(key).authenticated;
+      if (auth && Object.keys(auth).length !== 0) {
+        var authToken = auth.access_token;
+        var authType  = "Bearer";
+        settings.headers['Authorization'] = authType + " " + authToken;
+      }
+    }
+  }
+
+  try {
+    const response = await fetch(url, settings);
+    if (response.ok) {
+      response.json()
+        .then(function (data) {
+          console.log(data);
+          return data;
+        })
+        .catch(function (error) {
+          throw new Error('Fetch parsing error : ' + error.message);
+        });
+    } else {
+      throw new Error('Not handled exception !?');
+    }
+  } catch (error) {
+    throw error;
+  }
+};
