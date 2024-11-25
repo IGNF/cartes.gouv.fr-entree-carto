7dc9abbe502c5fe87dbb82ee89232f4fcb105dfa
jean-philippe bazonnais <jean-philippe.bazonnais@ign.fr>
2024-11-25 16:11:50 +0100

    Ajout des options filter sur le widget SearchEngine + Mise en vedette des couches dans l'editorial (#390)

--- src/stores/dataStore.js
+++ src/stores/dataStore.js
@@ -16,6 +16,7 @@ export const useDataStore = defineStore('data', () => {
   const m_tileMatrixSets = ref({});
   const m_contacts = ref({});
   const m_territories = ref([]);
+  const m_featured = ref([]);
   const isLoaded = ref(false);
   const error = ref("");
 
@@ -59,6 +60,7 @@ export const useDataStore = defineStore('data', () => {
       m_contacts.value = edito.contacts;
       m_informations.value = edito.informations;
       m_thematics.value = edito.thematics;
+      m_featured.value = edito.featured || [];
       m_layers.value = res;
       m_generalOptions.value = tech.generalOptions;
       m_tileMatrixSets.value = tech.tileMatrixSets;
@@ -87,6 +89,10 @@ export const useDataStore = defineStore('data', () => {
 
   function getThematics() {
     return m_thematics.value;
+  }
+
+  function getFeatured() {
+    return m_featured.value;
   }
 
   function getLayers() {
@@ -249,6 +255,7 @@ export const useDataStore = defineStore('data', () => {
     getContacts,
     getInformations,
     getThematics,
+    getFeatured,
     getLayers,
     getLayerKeysByID,
     getLayerIdByName,
