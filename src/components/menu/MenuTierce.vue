6f3b38767ff0dfabec119b4a689c06962856e642
hcaillaud <hector.caillaud@ign.fr>
2024-12-02 19:15:24 +0100

    icon dsfr importé

--- src/components/menu/MenuTierce.vue
+++ src/components/menu/MenuTierce.vue
@@ -13,10 +13,37 @@
 </script>
 
 <script setup lang="js">
+import { useMapStore } from "@/stores/mapStore"
+
+
+const mapStore = useMapStore();
+const map = mapStore.getMap();
+
+const openImport = () => {
+map.getControls().getArray().forEach(control => {
+  if (control.CLASSNAME == "ElevationPath") {
+    let button = [...control.element.children].filter(e => {
+      if (e.className.includes("GPshowOpen"))
+          return e
+      })
+      button[0].click()
+    }
+  })
+}
+
+const icon = "LayerImport"
+const defaultScale = 0.8325;
+const iconProps = computed(() => typeof icon === 'string'
+  ? { scale: defaultScale.value, name: icon }
+  : { scale: defaultScale.value, ...icon },
+);
 </script>
 
 <template>
-  <div>Importer des données</div>
+          
+  <div @click="openImport">
+    <VIcon v-bind="iconProps"/> Importer des données
+  </div>
   <hr>
   <div>Cartes enregistrées</div>
   <div>Vos contributions</div>
@@ -31,12 +58,12 @@
   <div>Aide</div>
   <hr>
   <div>Les nouvelles fonctionnalités</div>
-  <div>A propos</div>
+  <a href="/a-propos">A propos</a>
   <div>Accessiblité partiellement conforme</div>
   <div>Données personnelles</div>
   <div>Gestion des cookies</div>
   <div>Mention légales</div>
-  <div>Plan du site</div>
+  <a href="/plan-du-site">Plan du site</a>
 </template>
 
 <style scoped>
