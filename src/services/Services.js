13a8e6bc289f5d062b1d1b7c38562a2e9d7706b9
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-12-11 13:50:05 +0100

    Cohabiter les 2 modes d'authentification : local et remote (cartes.gouv.fr)

--- src/services/Services.js
+++ src/services/Services.js
@@ -11,7 +11,9 @@ import { useServiceStore } from '@/stores/serviceStore';
 const IAM_URL = import.meta.env.IAM_URL;
 const IAM_REALM = import.meta.env.IAM_REALM;
 const IAM_CLIENT_ID = import.meta.env.IAM_CLIENT_ID;
-const IAM_CLIENT_SECRET = import.meta.env.IAM_CLIENT_SECRET; 
+const IAM_CLIENT_SECRET = import.meta.env.IAM_CLIENT_SECRET;
+const IAM_AUTH_MODE = import.meta.env.IAM_AUTH_MODE;
+const IAM_REDIRECT_REMOTE = import.meta.env.IAM_REDIRECT_REMOTE;
 
 /**
  * @description 
@@ -59,7 +61,11 @@ class Services {
     this.user = options.user || {};
     /** erreurs IAM */
     this.error = options.error || {};
-    
+
+    /** mode local ou distant */
+    this.mode = options.mode || IAM_AUTH_MODE;
+    this.redirect = options.redirect || IAM_REDIRECT_REMOTE;
+
     // variables à instancier !
     this.#client = null;
     this.#fetchWrapper = null;
@@ -76,11 +82,13 @@ class Services {
    */
   #initialize (options) {
     this.url = encodeURI(location.origin + import.meta.env.BASE_URL);
+    var clientId = this.mode === "local" ? IAM_CLIENT_ID : null;
+    var clientSecret = this.mode === "local" ? IAM_CLIENT_SECRET : null;
     var settings = options.client ? options.client.settings : {
       server: `${IAM_URL}`,
 
-      clientId: `${IAM_CLIENT_ID}`,
-      clientSecret: `${IAM_CLIENT_SECRET}`,
+      clientId: `${clientId}`,
+      clientSecret: `${clientSecret}`,
       index: `${IAM_REALM}`,
 
       tokenEndpoint: `/realms/${IAM_REALM}/protocol/openid-connect/token`,
@@ -93,10 +101,13 @@ class Services {
     this.#fetchWrapper = new OAuth2Fetch({
       client: this.#client,
       getNewToken: async () => {
+        // FIXME
+        // comment faire un refresh token dans le mode distant ?
+        // il faudrait le code ?
         var token = await this.#client.authorizationCode.getToken({
-            code: this.code,
-            redirectUri: this.url,
-            code_verifier: this.codeVerifier
+          code: this.code,
+          redirectUri: this.url,
+          code_verifier: this.codeVerifier
         });
         return token;
       },
@@ -134,8 +145,8 @@ class Services {
   }
 
   /**
-   * Permet de valider la connexion en obtenant un token
-   * @returns {Promise} - statut : login / logout / null
+   * Permet de valider la connexion pour obtenir un token
+   * @returns {Promise} - statut : login / logout / error
    */
   isAccessValided () {
     var store = useServiceStore();
@@ -146,16 +157,17 @@ class Services {
     var code = urlParams.get('code');
     var session = urlParams.get('session_state');
     var error = urlParams.get('error');
+    var token = urlParams.get('token');
 
     var status = null;
-    // IAM login
+    // IAM login local
     if (code && session) {
       this.session = session;
       this.code = code;
       this.authenticated = true;
       status = "login";
     }
-    // IAM logout
+    // IAM logout local
     if (!code && session && session === this.session) {
       this.session = null;
       this.code = null;
@@ -165,6 +177,19 @@ class Services {
       this.user = {};
       this.error = {};
       status = "logout";
+    }
+    // IAM login distant
+    if (token) {
+      this.authenticated = true;
+      // INFO
+      // conversion de format de token
+      var t = JSON.parse(token);
+      this.token = {
+        accessToken : t.access_token,
+        expiresAt : t.expires, // FIXME refresh_expires_in ?
+        refreshToken : t.refresh_token
+      };
+      status = "login";
     }
     // Error
     if (error) {
