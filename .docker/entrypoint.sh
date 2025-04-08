5283609c7715a60528c376aa69610e4c75c3eb6b
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2025-04-08 14:13:23 +0200

    Fichier env dans un répertoire dédié

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Remplacer les placeholders dans env.js.template par les variables d'environnement
-envsubst < /usr/share/nginx/html/cartes/env.js.template > /usr/share/nginx/html/cartes/env.js
+envsubst < /usr/share/nginx/html/cartes/env/env.js.template > /usr/share/nginx/html/cartes/env/env.js
 
 set +x
 
