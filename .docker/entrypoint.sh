64cbe3e29858ca95ed9fbfb4061476edf56fab0a
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2025-04-10 11:37:48 +0200

    Utilisation du volume /tmp pour traitement et copie des env

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -x
-
 # Remplacer les placeholders dans env.js.template par les variables d'environnement
-envsubst < /usr/share/nginx/html/cartes/env/env.js.template > /usr/share/nginx/html/cartes/env/env.js
+envsubst < /tmp/env/env.js.template > /tmp/env/env.js
 
-set +x
+# Copie dans le volume (emptyDir)
+cp /tmp/env/env.js /usr/share/nginx/html/cartes/env/env.js
+cp /tmp/env/env.js.template /usr/share/nginx/html/cartes/env/env.js.template
 
-exit 0
+exec "$@"
