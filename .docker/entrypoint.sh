cc01523359f1d88aa6c3c56aa672d9e0bd2a3037
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2025-05-15 21:16:44 +0200

    Test en virant le user nginx

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Remplacer les placeholders dans env.js.template par les variables d'environnement
-envsubst < /app/dist/env/env.js.template > /app/dist/env/env.js
+envsubst < /tmp/env/env.js.template > /tmp/env/env.js
 
 # Copie dans le volume (emptyDir)
-cp /app/dist/env/env.js /usr/share/nginx/html/cartes/env/env.js
-cp /app/dist/env/env.js.template /usr/share/nginx/html/cartes/env/env.js.template
+cp /tmp/env/env.js /usr/share/nginx/html/cartes/env/env.js
+cp /tmp/env/env.js.template /usr/share/nginx/html/cartes/env/env.js.template
 
 exec "$@"
