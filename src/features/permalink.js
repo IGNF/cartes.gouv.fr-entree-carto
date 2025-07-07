bc0eb962ea18c9e99ff6325e034e4c4e5903e8f1
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2025-07-07 18:48:58 +0200

    Fix sur l'ouverture du permalien via les favoris

--- src/features/permalink.js
+++ src/features/permalink.js
@@ -8,27 +8,21 @@ import { useMapStore } from "@/stores/mapStore";
 */
 export const getLayersFromPermalink = (url) => {
   const store = useMapStore();
-  var lon = null;
-  var lat = null;
   // gestion des KVP dans l'URL (permalink)
   var params = useUrlParams(url);
   for (const key in params) {
     if (Object.prototype.hasOwnProperty.call(params, key)) {
       const value = params[key];
-      if (key === 'center') {
-        continue;
-      }
       if (key === 'permalink') {
         continue;
       }
-      // HACK : on deporte la maj du centre plus bas
-      // afin d'eviter trop de update du store !
+      if (key === 'center') {
+        continue;
+      }
       if (key === 'lon') {
-        lon = value;
         continue;
       }
       if (key === 'lat') {
-        lat = value;
         continue;
       }
       store[key] = value; // reactive !
@@ -37,7 +31,6 @@ export const getLayersFromPermalink = (url) => {
   // HACK : on force un rafraichissement de la carte
   setTimeout(() => {
     var map = store.map;
-    store.center = [lon, lat];
     map.getView().setZoom(store.zoom);
     map.getView().setCenter([store.x, store.y]);
   },0);
