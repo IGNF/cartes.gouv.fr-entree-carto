da0d9043378c6b0f9826976d8bf8710c2dd4f739
hcaillaud <hector.caillaud@ign.fr>
2025-12-22 12:13:09 +0100

    fix context menu mobile

--- src/components/carte/control/ContextMenu.vue
+++ src/components/carte/control/ContextMenu.vue
@@ -1,5 +1,6 @@
 <script setup lang="js">
 import { ContextMenu } from 'geopf-extensions-openlayers';
+import { useMatchMedia } from '@/composables/matchMedia';
 
 const props = defineProps({
   mapId: String,
@@ -8,12 +9,19 @@ const props = defineProps({
   contextMenuOptions: Object
 });
 
+const isSmallScreen = useMatchMedia('SM')
+
 const map = inject(props.mapId)
 const contextMenu = ref(new ContextMenu(props.contextMenuOptions));
-
+const pixel = ref([0,0])
 onMounted(() => {
   if (props.visibility) {
     map.addControl(contextMenu.value);
+    contextMenu.value.contextmenu.on("open", onContextMenuOpen)
+    map.on('singleclick', function (evt) {
+    const pixel = evt.pixel;
+      pixel.value =[90, pixel[1]]
+    });
   }
 })
 
@@ -25,6 +33,12 @@ onBeforeUpdate(() => {
     map.removeControl(contextMenu.value);
   }
 })
+
+const onContextMenuOpen = () => {
+  if (isSmallScreen.value) {
+    contextMenu.value.contextmenu.updatePosition([90, pixel[1]])
+  }
+}
 </script>
 
 <template>
