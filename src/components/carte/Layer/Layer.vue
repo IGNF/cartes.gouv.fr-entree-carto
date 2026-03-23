a30e99070ec0ab058428a7d52209dd8cf011cd30
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2026-03-23 13:39:05 +0100

    fix(Layer): ajouter un timestamp à l'URL pour le cache-busting

--- src/components/carte/Layer/Layer.vue
+++ src/components/carte/Layer/Layer.vue
@@ -139,6 +139,8 @@ onMounted(() => {
 
       log.debug("layer to add (bookmark)", name, type, format);
       var opts = props.layerOptions;
+      // HACK on ajoute un timestamp à l'url cache-busting
+      opts.url = `${opts.url}?_=${Date.now()}`;
       switch (type) {
         case "wms":
         case "wmts":
